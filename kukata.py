bc151215521305f53a7400e33d96a588b76966c7
Viktor <vnb806@gmail.com>
2025-05-28 02:01:42 +0300

    add height

--- kukata.py
+++ kukata.py
@@ -3,5 +3,6 @@ first_name = input()
 last_name = input()
 age = int(input())
 town = input()
+height = input()
 
-print(f"You are {first_name} {last_name}, a {age}-years old beast from {town}.")   
+print(f"You are {first_name} {last_name}, a {age}-years old beast from {town} which is tall {height}.")
